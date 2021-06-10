7b932c00c412db75a117a590089ab63612708f77
vishal-i <vishalingle123@gmail.com>
2021-06-11 02:36:18 +0530

    Add ability to create new articles from UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,21 @@ class ArticlesController < ApplicationController
         @articles= Article.all 
     end
 
+    def new
+
+    end
+
+    def create 
+    #  render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: params[:article]
+    #render plain: @article
+    #render plain: @article.inspect
+    @article.save
+    #render 'new'
+   redirect_to @article
+  # puts @article.errors.full_messages
+    
+    end
+
 end
