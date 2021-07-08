31c683faffbdad3faa146c1f6fc2cd686af87a5c
vishal-i <vishalingle123@gmail.com>
2021-07-09 00:39:08 +0530

    Add one to many association between users and articles from back end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,7 @@ class ArticlesController < ApplicationController
     def create 
     #  render plain: params[:article]
     @article = Article.new(article_params)
+    @article.user = User.first
     #render plain: params[:article]
     #render plain: @article
     #render plain: @article.inspect
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210623110654) do
+ActiveRecord::Schema.define(version: 20210629060245) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
