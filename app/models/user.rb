8a318a70c9bd8577645b1dbe0b76174fe14e658d
vishal-i <vishalingle123@gmail.com>
2021-07-10 12:56:47 +0530

    Add bcrypt and has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
                length: {maximum: 105 },
                format: {with: VALID_EMAIL_REGEX}
 
+has_secure_password
+
 end
